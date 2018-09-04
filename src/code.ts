65b37b92a8d5e19cb0af69940762b545902d29ce
Raoul1996 <baobaozhou199606@gmail.com>
2018-09-04 10:26:41 +0800

    add speech api, but it seems not work currently
    Change-Id: I08cf95309595d24707bf45b2d7d8d766a03fd7cc

--- src/code.ts
+++ src/code.ts
@@ -1,14 +1,68 @@
-import {fromEvent} from 'rxjs'
-import {throttle, scan, map} from 'rxjs/operators'
-import {interval} from 'rxjs'
+import {Observable} from 'rxjs'
+import 'rxjs/add/observable/of'
+import 'rxjs/add/operator/map'
+
+const URL = 'https://api.raoul1996.cn/login';
+const stream$ = Observable.create((observer: any) => {
+    let request = fetch(URL, {
+        method: 'POST',
+        headers: new Headers({
+            'Content-Type': "application/json"
+        }),
+    })
+        .then((res: any) => res.json())
+        .then((response: string) => {
+            observer.next(response);
+            observer.complete();
+        }).catch((err: any) => {
+            observer.error(err);
+        })
+});
+const subscription = stream$.subscribe(
+    (data: string) => console.log(data),
+    (err: any) => console.log(`Error:${err}`),
+    () => console.log(`Complete: no args`));
+
+console.clear();
+
+export interface IWindow extends Window {
+    webkitSpeechRecognition: any;
+}
+
+const {webkitSpeechRecognition}: IWindow = <IWindow>window;
+const speechRecognition$ = new Observable((observable: any) => {
+    const recognition = new webkitSpeechRecognition();
+    recognition.onresult = (event: any) => {
+        observable.next(event);
+        observable.complete();
+    };
+    recognition.start();
+    return () => {
+        recognition.stop();
+    }
+});
+
+const say = (text: string) => new Observable((observer: any) => {
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.onend = ((e: any) => {
+        observer.next(e);
+        observer.complete();
+    });
+    speechSynthesis.speak(utterance);
+});
 
 const button = document.createElement('button');
-button.appendChild(document.createTextNode('button'));
+button.appendChild(document.createTextNode('voice'));
 document.querySelector('#output').appendChild(button);
-fromEvent(button, 'click')
-    .pipe(
-        throttle((e: any) => interval(1000)),
-        map((e: any) => e.clientX),
-        scan((count: number, clientX: number) => count + clientX, 0)
-    )
-    .subscribe((count) => console.log(`${count}`));
+
+const heyClick$ = Observable.fromEvent(button, 'click');
+
+heyClick$
+    .switchMap((e: any) => speechRecognition$)
+    .map((e: any) => e.results[0][0].transcript)
+    .map((text: string) => {
+        console.log(text);
+        return text;
+    })
+    .concatMap(say)
+    .subscribe((e: any) => console.log(e));
